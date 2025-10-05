f6334a2d5855f8f70a3ecbcf5954bfc534cfd1e3
RyanStudioo <150920961+RyanStudioo@users.noreply.github.com>
2025-10-05 13:28:43 +0800

    example fixes, kwarg fixes

--- tests/test.py
+++ tests/test.py
@@ -1,19 +1,39 @@
 
 from parsegument.CommandGroup import CommandGroup
 from parsegument.Command import Command
-from src.parsegument import Parsegumenter
+from parsegument.Arguments import Argument, Flag, Operand
+from parsegument import Parsegumenter
 
-def foo(bar):
-    print(bar)
+def foo(bar, bar2, reverse:bool=False):
+    if reverse:
+        print(f"{bar} {bar2}"[::-1])
+    print(bar, bar2)
+
+def another(argument):
+    print(argument[::-1])
 
 parsegumenter = Parsegumenter()
+
 main_command = CommandGroup("Main")
-sub_command = CommandGroup("Sub")
-command = Command("foo", foo)
-main_command.add_child(sub_command)
-sub_command.add_child(command)
 parsegumenter.add_child(main_command)
 
-parsegumenter.execute("Main Sub foo yes")
+sub_command = CommandGroup("Sub")
+main_command.add_child(sub_command)
+
+command = Command("foo", foo)
+sub_command.add_child(command)
+command.add_node(Argument(name="bar", arg_type=str))
+command.add_node(Argument(name="bar2", arg_type=str))
+command.add_node(Flag(name="reverse"))
+
+sub_sub_command = CommandGroup("SubSub")
+sub_command.add_child(sub_sub_command)
+
+another_command = Command("Another", another)
+sub_sub_command.add_child(another_command)
+
+parsegumenter.execute("Main Sub foo yes yes2")
+parsegumenter.execute("Main Sub foo yes yes2 -reverse")
+parsegumenter.execute("Main Sub SubSub Another yessir")
 
 
