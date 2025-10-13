1ede18860253a6db85f6469d79da49fa880a6acf
RyanStudioo <150920961+RyanStudioo@users.noreply.github.com>
2025-10-13 17:40:38 +0800

    CommandGroup and Parsegumented based on BaseGroup

--- src/parsegument/CommandGroup.py
+++ src/parsegument/CommandGroup.py
@@ -1,31 +1,14 @@
 import typing
 from typing import Union, Callable, get_origin
-from inspect import signature
 from .Node import Node
 from .Command import Command
+from BaseGroup import BaseGroup
 from .error import NodeDoesNotExist
-from .utils.convert_params import convert_param
 
-class CommandGroup(Node):
+class CommandGroup(BaseGroup):
     def __init__(self, name: str) -> None:
         super().__init__(name)
         self.children = {}
-
-    def add_child(self, node:Union[Command, "CommandGroup"]) -> None:
-        self.children[node.name] = node
-
-    def command(self, func: Callable):
-        def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
-
-        params = signature(func).parameters
-        func_name = func.__name__
-        command_object = Command(name=func_name, executable=func)
-        for key, param in params.items():
-            converted = convert_param(param)
-            command_object.add_node(converted)
-        self.add_child(command_object)
-        return wrapper
 
     def execute(self, nodes:list[str]):
         child = self.children.get(nodes[0])
--- src/parsegument/Parsegumenter.py
+++ src/parsegument/Parsegumenter.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from typing import Union
+from typing import Union, Any
+from .BaseGroup import BaseGroup
 from .error import NodeDoesNotExist, ArgumentGroupNotFound, MultipleChildrenFound
 import shlex
 
@@ -9,23 +10,28 @@ if annotations:
     from .Command import Command
 
 
-class Parsegumenter:
-    def __init__(self) -> None:
+class Parsegumenter(BaseGroup):
+    def __init__(self, name:str="", prefix:str="") -> None:
+        super().__init__(name)
         self.children = {}
+        self.prefix = prefix
 
+    def _check_valid(self, command: list[str]) -> bool:
+        first = command[0]
+        if self.prefix and not first.startswith(self.prefix): return False
+        else: first = first[len(self.prefix):]
+        if self.name and first != self.name: return False
+        else: return True
 
-    def add_child(self, child: Union[CommandGroup, Command]):
-        if child.name in [i.name for i in self.children]:
-            return False
-        self.children[child.name] = child
-        return True
-
-    def execute(self, command:Union[str, list[str]]):
+    def execute(self, command:Union[str, list[str]]) -> Union[Any, None]:
         parsed = shlex.split(command) if isinstance(command, str) else command
+        if not self._check_valid(parsed): return None
+        if self.name: parsed.pop(0)
         child_name = parsed[0]
         arguments = parsed[1:]
         child_command = self.children.get(child_name)
         if not child_command:
             raise NodeDoesNotExist
         return child_command.execute(arguments)
+
 
