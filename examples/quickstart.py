5478f467d8752d2eff0aff32cfa373878ffdeb27
RyanStudioo <150920961+RyanStudioo@users.noreply.github.com>
2025-10-13 19:46:07 +0800

    quickstart

--- examples/quickstart.py
+++ examples/quickstart.py
@@ -4,8 +4,8 @@ parser = pg.Parsegumenter() # Create Parsegumenter
 group1 = pg.CommandGroup("group1") # Define a Group
 parser.add_child(group1) # Add the Group to the main Parsegumenter
 
-@group1.command
+@group1.command # Add decorator to create command
 def foo(bar:str):
     print(bar)
 
-parser.execute("group1 foo bar_string")
+parser.execute("group1 foo bar_string") # Execute string
