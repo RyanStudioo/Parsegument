8f2500821e253dbb49223337d098d033d0ebdcd9
RyanStudioo <150920961+RyanStudioo@users.noreply.github.com>
2025-10-19 05:41:50 +0800

    private func + function docs

--- src/parsegument/decorators/parameters.py
+++ src/parsegument/decorators/parameters.py
@@ -4,11 +4,14 @@ from typing import Optional
 from ..Parameters import Argument, Flag, Operand
 from ..error import ParameterNotFound, CommandNotFound
 
-def check_if_param_exists(func, name):
+def _check_if_param_exists(func, name):
     signature = inspect.signature(func)
     return name in signature.parameters
 
 def argument(name: str, arg_type: Optional[type]=None):
+    """
+    Decorator function to modify an argument's information
+    """
     def decorator(func):
 
         @functools.wraps(func)
@@ -17,7 +20,7 @@ def argument(name: str, arg_type: Optional[type]=None):
 
         if not hasattr(func, "__parameters__"):
             func.__parameters__ = {"args": {}, "kwargs": {}}
-        if not check_if_param_exists(func, name):
+        if not _check_if_param_exists(func, name):
             raise ParameterNotFound(name)
         func.__parameters__["args"][name] = Argument(name, arg_type)
         wrapper.__parameters__ = func.__parameters__
@@ -26,13 +29,16 @@ def argument(name: str, arg_type: Optional[type]=None):
     return decorator
 
 def flag(name: str):
+    """
+    Decorator function to modify a flag's information
+    """
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             return func(*args, **kwargs)
         if not hasattr(func, "__parameters__"):
             func.__parameters__ = {"args": {}, "kwargs": {}}
-        if not check_if_param_exists(func, name):
+        if not _check_if_param_exists(func, name):
             raise ParameterNotFound(name)
         func.__parameters__["kwargs"][name] = Flag(name)
         wrapper.__parameters__ = func.__parameters__
@@ -41,13 +47,16 @@ def flag(name: str):
     return decorator
 
 def operand(name: str, arg_type: Optional[type]=None):
+    """
+    Decorator function to modify an operand's information
+    """
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             return func(*args, **kwargs)
         if not hasattr(func, "__parameters__"):
             func.__parameters__ = {"args": {}, "kwargs": {}}
-        if not check_if_param_exists(func, name):
+        if not _check_if_param_exists(func, name):
             raise ParameterNotFound(name)
         func.__parameters__["kwargs"][name] = Operand(name, arg_type)
         wrapper.__parameters__ = func.__parameters__
