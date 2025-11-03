c703b3629573f024e62ec97b729cb8b863e2f9d1
RyanStudioo2 <150920961+RyanStudioo@users.noreply.github.com>
2025-11-03 09:23:51 +0800

    tests

--- tests/test2.py
+++ tests/test2.py
@@ -1,3 +1,5 @@
+from typing import Optional, Any
+
 import parsegument as pg
 from parsegument import CommandGroup
 
@@ -7,15 +9,17 @@ class ChildGroup(CommandGroup):
 
     @staticmethod
     @pg.argument("test", int)
-    def method_thing(test:str):
+    def method_thing(test:str) -> Optional[Any]:
         if type(test) == str:
             return test + ", This is a method thing"
         elif type(test) == int:
             return test + 2
+        return None
+
 
 parser = pg.Parsegumenter()
 group = ChildGroup()
 group.initialise()
 parser.add_child(group)
-print(parser.execute("ChildGroup method_thing 10"))
+print(parser.execute("ChildGroup method_thing 10.5"))
 print(parser.schema)
