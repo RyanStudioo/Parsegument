81905279cf4fc6967abb8499b8537a5591a7abc9
RyanStudioo <150920961+RyanStudioo@users.noreply.github.com>
2025-11-08 21:59:10 +0800

    untrack .idea

--- examples/quickstart.py
+++
@@ -1,8 +0,0 @@
-import parsegument as pg
-parser = pg.Parsegumenter(name="quickstart") # Create Parsegumenter
-
-@parser.command() # Add decorator to create command
-def foo(bar:str):
-    print(bar)
-
-parser.execute("quickstart foo bar_string") # Execute string
--- src/parsegument/types/ArgDict.py
+++
@@ -1,7 +0,0 @@
-from typing import Union, Dict, TypedDict
-
-ArgType = Union["Argument", "Operand", "Flag", None]
-
-class ArgDict(TypedDict):
-    args: Dict[str, ArgType]
-    kwargs: Dict[str, ArgType]
